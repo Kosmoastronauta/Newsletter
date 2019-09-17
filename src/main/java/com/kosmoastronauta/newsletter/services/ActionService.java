3a069b99dcbc8673f745e2f620cd49b889edbadb
Mateusz Gil <mateusz_gil@outlook.com>
2019-09-17 15:29:52 +0200

    Added action validating and adding new in service

--- src/main/java/com/kosmoastronauta/newsletter/services/ActionService.java
+++ src/main/java/com/kosmoastronauta/newsletter/services/ActionService.java
@@ -1,10 +1,13 @@
 package com.kosmoastronauta.newsletter.services;
 
 import com.kosmoastronauta.newsletter.domain.Action;
+import com.kosmoastronauta.newsletter.domain.EmailGroup;
 import com.kosmoastronauta.newsletter.repository.ActionRepository;
+import com.kosmoastronauta.newsletter.repository.EmailGroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 @Service
@@ -12,6 +15,9 @@ public class ActionService
 {
     @Autowired
     ActionRepository actionRepository;
+
+    @Autowired
+    EmailGroupRepository emailGroupRepository;
 
     public List<Action> getAllActionsByGroupId(long groupId) throws NoSuchFieldException
     {
@@ -21,5 +27,21 @@ public class ActionService
             throw new NoSuchFieldException("There is no actions for group with that id");
 
         else return actions;
+    }
+
+    public void addActionForGroup(Action action) throws NoSuchFieldException,InvalidParameterException
+    {
+        if(!isActionValid(action)) throw new InvalidParameterException("Invalid data!");
+
+        if(!emailGroupRepository.existsById(action.getGroupId()))
+            throw new NoSuchFieldException("There is no group with that id!");
+
+        actionRepository.save(action);
+    }
+
+    private boolean isActionValid(Action action)
+    {
+        // if any of necessary field is empty
+        return action.getGroupId() != 0 && !action.getName().isEmpty() && !action.getSchema().isEmpty();
     }
 }
