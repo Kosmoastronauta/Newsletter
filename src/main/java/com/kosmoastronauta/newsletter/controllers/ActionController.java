7d03e56992eb3cb3f0eba40933a9757ebc3d4a76
Mateusz Gil <mateusz_gil@outlook.com>
2019-09-17 15:36:40 +0200

    Added post mapping for adding new action to group

--- src/main/java/com/kosmoastronauta/newsletter/controllers/ActionController.java
+++ src/main/java/com/kosmoastronauta/newsletter/controllers/ActionController.java
@@ -5,10 +5,9 @@ import com.kosmoastronauta.newsletter.services.ActionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 @RestController
@@ -17,7 +16,7 @@ public class ActionController
     @Autowired
     ActionService actionService;
 
-    @GetMapping(path = "/group/{groupId}/actions/")
+    @GetMapping(path = "/groupId/{groupId}/actions/")
     public ResponseEntity<List<Action>> getAllActionsByGroupId(@PathVariable long groupId)
     {
         List<Action> actions;
@@ -29,5 +28,23 @@ public class ActionController
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(actions, HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/addActionToGroup/")
+    public ResponseEntity<HttpStatus> addActionToGroupById(@RequestBody Action action)
+    {
+        try
+        {
+            actionService.addActionForGroup(action);
+        }catch(InvalidParameterException e)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        catch(NoSuchFieldException e)
+        {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
