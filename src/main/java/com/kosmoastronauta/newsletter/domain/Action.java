ff8a29f48f2abeef3b476699ca5a718991f767be
Mateusz Gil <mateusz_gil@outlook.com>
2019-09-17 14:25:38 +0200

    Created Action entity

---
+++ src/main/java/com/kosmoastronauta/newsletter/domain/Action.java
@@ -0,0 +1,50 @@
+package com.kosmoastronauta.newsletter.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Action
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private long groupId;
+    private String name;
+    private String schema;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSchema() {
+        return schema;
+    }
+
+    public void setSchema(String schema) {
+        this.schema = schema;
+    }
+}
